b7e427b4965b08266b333d2e9a14c2a2be6206c9
Rafael Colombo de Souza <rcolombodesouza@gmail.com>
2017-05-24 13:45:29 -0300

    Add files via upload

---
+++ search-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css']
+})
+export class SearchResultComponent {
+  
+  @Input() _data: JSON;
+  
+  constructor() { }
+
+
+}
